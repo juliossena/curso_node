43e8eb2063956df2a53a3267de15125dc1611fd3
juliossena <juliosouzasena@gmail.com>
2023-06-14 19:33:50 -0300

    feat: insert prettier and eslint

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello World")
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
-app.get("/user/:nome/sobrenome/:sobrenome", function(req, res) {
-    res.send({
-        nome: req.params.nome,
-        sobrenome: req.params.sobrenome,
-    })
+app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
+  res.send({
+    nome: req.params.nome,
+    sobrenome: req.params.sobrenome,
+  });
 });
 
-app.listen(8080, function(){
-    console.log("Servidor rodando")
-})
+app.listen(8080, function () {
+  console.log('Servidor rodando');
+});

e2edd2e845f7691231e1485a647d715b3d852475
juliossena <juliosouzasena@gmail.com>
2023-06-15 19:34:09 -0300

    feat: insert prisma in project

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const userRouter = Router();
 
@@ -6,8 +7,10 @@ const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', function (req, res) {
-  res.send('Hello World USER AGORA');
+router.get('/', async function (req, res) {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  res.send(users);
 });
 
 router.get('/:nome', function (req, res) {

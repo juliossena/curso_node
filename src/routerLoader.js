39251c863284a9cd8bfe65418336af31824bac22
juliossena <juliosouzasena@gmail.com>
2023-06-15 19:06:53 -0300

    fix

--- src/routerLoader.js
+++ src/routerLoader.js
@@ -6,26 +6,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const routerLoader = (app) => {
-    const modulesPath = path.join(__dirname, 'modules');
+  const modulesPath = path.join(__dirname, 'modules');
 
-    fs.readdirSync(modulesPath).forEach(async (dir) => {
-        const modulePath = path.join(modulesPath, dir);
+  fs.readdirSync(modulesPath).forEach(async (dir) => {
+    const modulePath = path.join(modulesPath, dir);
 
-        if(fs.statSync(modulePath).isDirectory()) {
-            const controllerPath = path.join(modulePath, `${dir}.controller.js`);
+    if (fs.statSync(modulePath).isDirectory()) {
+      const controllerPath = path.join(modulePath, `${dir}.controller.js`);
 
-            if(fs.existsSync(controllerPath)) {
-                const controller = await import(controllerPath);
+      if (fs.existsSync(controllerPath)) {
+        const controller = await import(controllerPath);
 
-                if(controller.default && typeof controller.default === 'function') {
-                    app.use(controller.default);
-                }
-            }
+        if (controller.default && typeof controller.default === 'function') {
+          app.use(controller.default);
         }
-    })
-
-    
-
-}
-
-
+      }
+    }
+  });
+};

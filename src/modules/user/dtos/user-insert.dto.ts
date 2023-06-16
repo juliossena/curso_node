e75606bc8adc73c21be36929e3e5637265bf22c5
juliossena <juliosouzasena@gmail.com>
2023-06-15 22:11:55 -0300

    feat: insert type in user.service

--- src/modules/user/dtos/user-insert.dto.ts
+++ src/modules/user/dtos/user-insert.dto.ts
@@ -1,11 +1,8 @@
-export interface UserModel {
-  id: number;
+export interface UserInsertDTO {
   name: string;
   cpf: string;
   email: string;
   password: string;
   typeUser: number;
   phone: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { UserModel } from './user.model';
+import { UserInsertDTO } from './dtos/user-insert.dto';
 
 const prisma = new PrismaClient();
 
-export const getUsers = async () => {
+export const getUsers = async (): Promise<UserModel[]> => {
   return prisma.user.findMany();
 };
 
-export const createUser = async (body) => {
+export const createUser = async (body: UserInsertDTO): Promise<UserModel> => {
   return prisma.user.create({
     data: body,
   });

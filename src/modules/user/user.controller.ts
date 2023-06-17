072344f0ba86023e10af55b0efe62341ec071196
juliossena <juliosouzasena@gmail.com>
2023-06-17 12:02:44 -0300

    feat: insert exception in service and controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { createUser, getUsers } from './user.service';
 import { UserInsertDTO } from './dtos/user-insert.dto';
+import { NotFoundException } from '@exceptions/not-found-exception';
 
 const userRouter = Router();
 
@@ -9,7 +10,13 @@ const router = Router();
 userRouter.use('/user', router);
 
 router.get('/', async (_, res: Response): Promise<void> => {
-  const users = await getUsers();
+  const users = await getUsers().catch((error) => {
+    if (error instanceof NotFoundException) {
+      res.status(204);
+    } else {
+      res.status(500).send(error.message);
+    }
+  });
   res.send(users);
 });
 
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 import { UserModel } from './user.model';
 import { UserInsertDTO } from './dtos/user-insert.dto';
+import { NotFoundException } from '@exceptions/not-found-exception';
+import { InternalServerErrorException } from '@exceptions/internal-server-error-exception';
 
 const prisma = new PrismaClient();
 
 export const getUsers = async (): Promise<UserModel[]> => {
-  return prisma.user.findMany();
+  const users = await prisma.user.findMany();
+
+  if (users?.length === 0) {
+    throw new NotFoundException('User');
+  }
+
+  return users;
 };
 
 export const createUser = async (body: UserInsertDTO): Promise<UserModel> => {

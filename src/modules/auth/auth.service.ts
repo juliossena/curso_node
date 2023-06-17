5c64d9001ce118713edcb53df89a487d8938b5ba
juliossena <juliosouzasena@gmail.com>
2023-06-17 13:53:24 -0300

    feat: validate send password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { UserModel } from '@modules/user/user.model';
 import { AuthDTO } from './dtos/auth.dto';
 import { getUserByEmail } from '@modules/user/user.service';
+import { validatePassword } from '@utils/password';
+import { NotFoundException } from '@exceptions/not-found-exception';
 
 export const validateAuth = async (authDto: AuthDTO): Promise<UserModel> => {
-  const user = getUserByEmail(authDto.email);
+  const user = await getUserByEmail(authDto.email);
+
+  const isValidPassword = await validatePassword(authDto.password, user.password);
+
+  if (!isValidPassword) {
+    throw new NotFoundException('User');
+  }
 
   return user;
 };

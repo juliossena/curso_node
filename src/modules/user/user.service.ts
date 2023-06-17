e9fcdc0168fa69e27b7a61f432facfff5e39cc54
juliossena <juliosouzasena@gmail.com>
2023-06-17 12:44:04 -0300

    feat: insert validation in create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { UserModel } from './user.model';
 import { UserInsertDTO } from './dtos/user-insert.dto';
 import { NotFoundException } from '@exceptions/not-found-exception';
+import { BadRequestException } from '@exceptions/bad-request-exception';
 
 const prisma = new PrismaClient();
 
@@ -15,7 +16,47 @@ export const getUsers = async (): Promise<UserModel[]> => {
   return users;
 };
 
+export const getUserByEmail = async (email: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('User');
+  }
+
+  return user;
+};
+
+export const getUserByCpf = async (cpf: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      cpf,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('User');
+  }
+
+  return user;
+};
+
 export const createUser = async (body: UserInsertDTO): Promise<UserModel> => {
+  const userEmail = await getUserByEmail(body.email).catch(() => undefined);
+
+  if (userEmail) {
+    throw new BadRequestException('Email exist in DB');
+  }
+
+  const userCpf = await getUserByCpf(body.cpf).catch(() => undefined);
+
+  if (userCpf) {
+    throw new BadRequestException('Cpf exist in DB');
+  }
+
   return prisma.user.create({
     data: body,
   });

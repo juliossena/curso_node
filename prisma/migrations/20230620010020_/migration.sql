e1eb1eac0de9dc1a51c999e9a89bd9f518407705
juliossena <juliosouzasena@gmail.com>
2023-06-19 22:01:48 -0300

    feat: create migration to state and city

---
+++ prisma/migrations/20230620010020_/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "State" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "State_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "stateId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "City" ADD CONSTRAINT "City_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
